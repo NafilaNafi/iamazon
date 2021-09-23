0ef5ab3f3f42092ce3123bc9f43041656ba575ee
NafilaNafi <Nafila@outlook.com>
2021-09-23 11:29:30 +0530

    Homepage & checkout page setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCh4GWRDzLmRG8CYuJ35E_xv_AcaWfBl_s",
+  authDomain: "challenge-ca335.firebaseapp.com",
+  projectId: "challenge-ca335",
+  storageBucket: "challenge-ca335.appspot.com",
+  messagingSenderId: "337446976452",
+  appId: "1:337446976452:web:b45ee4cf475fe0210e8291",
+  measurementId: "G-5TH8QFX5BL",
+};
